352d73280f3dd1eb45215a0dfc8406fbfe6f3b1d
Christopher D'Angelo <chris@mathhead200.com>
2025-12-05 11:31:18 -0500

    Small updates
    Last name still not working

--- final/www/receipts.js
+++ final/www/receipts.js
@@ -3,13 +3,35 @@ const cfnames = document.querySelector("#cfnames");
 const clnames = document.querySelector("#clnames");
 const cfname = document.querySelector("#cfname");
 const clname = document.querySelector("#clname");
-function cfnamesUpdated(event) {
-	let select = event.currentTarget;
-	let option = select.options[select.selectedIndex];
-	cid.value = option.dataset.cid;
-	console.log(select, option, cid.value);
+
+function cidUpdated() {
+	for (let cnames of [cfnames, clnames]) {  // for both #cfanmes and #clnames <select>
+		cnames.value = "";
+		for (let option of cnames.options)
+			if (option.dataset.cid === cid.value) {
+				cnames.value = option.value;
+				break;
+			}
+	}
 }
-cfnames.addEventListener("input", cfnamesUpdated);
-cfnames.addEventListener("change", cfnamesUpdated);
+
+function cfnamesUpdated(select) {
+	let option = select.options[select.selectedIndex];
+	cid.value = option.dataset.cid ?? "";
+}
+
+// cid.addEventListener("input", cidUpdated);
+cid.addEventListener("change", cidUpdated);
+cfnames.addEventListener("input", event => cfnamesUpdated(event.currentTarget));
+cfnames.addEventListener("change", event => cfnamesUpdated(event.currentTarget));
+
+// fill inputs onload with query string
+let args = new URLSearchParams(window.location.search);
+if (args.has("cid")) {
+	cid.value = args.get("cid");
+	cidUpdated();
+}
+for (let name of ["dstart", "dend"])
+	document.querySelector(`input[name=${name}]`).value = args.get(name) ?? "";
 
 // TODO: add more functionality, like disable on custom search, etc.
